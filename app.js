33aa94978f76a92f5b4980f943627af46d872d9a
IbrahimT <ibrahim.thaanbir@gmail.com>
2020-04-13 12:45:19 +0100

    ep10 - dynamic css

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 25,
-        x: 0,
-        y: 0,
-        name: '',
-        age: '',
-        a: 0,
-        b: 0
+        available: false,
+        nearby: false
     },
     methods: {
-        add: function(inc){
-            this.age += inc;
-        },
-        subtract: function(dec) {
-            this.age -= dec;
-        },
-        updateXY: function() {
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function() {
-            alert('You clicked me');
-        },
-        logName: function() {
-            console.log('you entered your name');
-        },
-        logAge: function() {
-            console.log('you entered your age');
-        }
+
     },
     computed: {
-        addToA: function () {
-            console.log('addTOa');
-            return this.a + this.age;
-        },
-        addToB: function () {
-            console.log('addTOb');
-            return this.b + this.age;
+        compClasses: function() {
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 });
