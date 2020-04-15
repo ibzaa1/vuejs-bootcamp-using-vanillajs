393a93d5e49864f698e31be0df3ce2e70759211b
IbrahimT <ibrahim.thaanbir@gmail.com>
2020-04-15 08:17:15 +0100

    ep15 - Components

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+Vue.component('greeting', {
+    template: '<p>Hey there i am {{ name }}. <button v-on:click="changeName">Change Name</button></p>',
+    data: function() {
+        return {
+            name: 'Ibby'
+        }
+    },
+    methods: {
+        changeName: function () {
+            this.name = 'Siam';
+        }
+    }
+});
+
 var one = new Vue({
     el: '#vue-app-one',
     data: {
