255442b3b2a4345a325ac798589cd04b07b4eff4
IbrahimT <ibrahim.thaanbir@gmail.com>
2020-04-15 08:07:30 +0100

    ep14 - multiple Vue instances

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-new Vue({
-    el: '#vue-app',
+var one = new Vue({
+    el: '#vue-app-one',
     data: {
-        error: false,
-        success: false
+        title: 'Vue app one'
     },
     methods: {
 
     },
     computed: {
-
+        greet: function () {
+            return 'Hello from app 1';
+        }
     }
 });
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue app two'
+    },
+    methods: {
+        changeTitle: function () {
+            one.title = 'title changed';
+        }
+    },
+    computed: {
+        greet: function () {
+            return 'Hello from app 2';
+        }
+    }
+});
+
+two.title = "changed from outside;"
